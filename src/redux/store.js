cdd3a46ba7360aacb39c631bf1ba82c860f61f1e
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-12-19 17:08:18 +0100

    prevent redux action login in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
-const middleWares = [ logger ];
+const middleWares = process.env.NODE_ENV === 'development' ? [ logger ] : [];
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 export const persistor = persistStore(store);
